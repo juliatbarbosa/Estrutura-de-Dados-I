618eb7da3748b69a90e8675b9144de75666816bd
Julia Teixeira Barbosa <juliateixeira22@hotmail.com>
2023-08-29 21:39:21 -0300

    aula

---
+++ 29082023/aula.py
@@ -0,0 +1,28 @@
+""" 
+[codigo, nome, descricao, preco]
+ATRIBUTOS OU CARACTERISTICAS--›codigo, nome, descricão, preco
+CHAVE-> codigo
+VALOR-> nome, descricao, preco. Vamos selecionar uma caracteristica nome.
+codigo: nome
+ """
+estoque = {} #Ed dicionario
+opcao = 1
+while opcao != 3:
+    print('=' * 10)
+    print('===MENU===')
+    print('1- Adicionar')
+    print('2- Consultar')
+    print('3- Sair')
+    opcao =int(input('>>>>'))
+    if opcao == 1:
+        codigo = input('Código: ')
+        nome = input('Nome do produto: ')
+        estoque[codigo] = nome
+        print('Adicionado com sucesso!')
+    elif opcao == 2:
+        #vou informar o codigo e ele irá me retornar o nome
+        codigo = input('Código desejado: ')
+        registro = estoque[codigo]
+        print(f'REGISTRO RECUPERADO: {registro}')
+    elif opcao == 3:
+        print('saindo....')
