ed8117b1c7e6bdb22a80c008ab2d12982f934f60
Julia Teixeira Barbosa <juliateixeira22@hotmail.com>
2023-11-16 20:42:43 -0300

    exercicios

---
+++ 2º bimestre/14112023/arquivos/exercicio.py
@@ -0,0 +1,9 @@
+with open ('C:\\Users\\teixe\\OneDrive\\Documentos\\FACULDADE\\2º semestre\\Estrutura de Dados I\\2º bimestre\\14112023\\arquivos\\entrada.txt', 'r') as arquivo:
+    num1 = int(arquivo.readline())
+    num2 = int(arquivo.readline())
+
+soma = num1 + num2
+print(soma)
+
+
+
